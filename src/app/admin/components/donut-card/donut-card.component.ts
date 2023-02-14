fcd02f37d538765b0ef4ad46df7a417775f95821
Jamie Thompson <jamieraethompson@gmail.com>
2023-02-14 16:11:51 -0600

    set up admin module, starting components and Donut model

---
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donut-card',
+  templateUrl: './donut-card.component.html',
+  styleUrls: ['./donut-card.component.scss']
+})
+export class DonutCardComponent {
+
+}
---
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donut-list',
+  templateUrl: './donut-list.component.html',
+  styleUrls: ['./donut-list.component.scss']
+})
+export class DonutListComponent {
+
+}
