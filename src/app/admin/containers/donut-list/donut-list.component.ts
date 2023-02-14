deeed5afe43f8a2d8d9769596f035e0a06f6bb10
Jamie Thompson <jamieraethompson@gmail.com>
2023-02-14 16:14:29 -0600

    set up temporary dummy data to render donut list

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Donut } from '../../models/donut.model';
 
 @Component({
   selector: 'app-donut-list',
   templateUrl: './donut-list.component.html',
-  styleUrls: ['./donut-list.component.scss']
+  styleUrls: ['./donut-list.component.scss'],
 })
-export class DonutListComponent {
+export class DonutListComponent implements OnInit {
+  donuts!: Donut[];
 
+  ngOnInit(): void {
+    this.donuts = [
+      {
+        id: 'y8z0As',
+        name: 'Just Chocolate',
+        icon: 'just-chocolate',
+        price: 119,
+        promo: true,
+        description: 'For the pure chocoholic',
+      },
+      {
+        id: '3u98Kl',
+        name: 'Glazed Fudge',
+        icon: 'glazed-fudge',
+        price: 129,
+        description: 'Sticky perfection',
+      },
+      {
+        id: 'ae098s',
+        name: 'Caramel Swirl',
+        icon: 'caramel-swirl',
+        price: 129,
+        description: 'Chocolate drizzled with caramel',
+      },
+    ];
+  }
 }
