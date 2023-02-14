3614d7601c1fe1b09b704433a5a75769c309f10b
Jamie Thompson <jamieraethompson@gmail.com>
2023-02-14 16:24:29 -0600

    set up card layout and styling, set up data flow from container component, formatted donut price field

--- src/app/admin/components/donut-card/donut-card.component.ts
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Donut } from '../../models/donut.model';
 
 @Component({
   selector: 'app-donut-card',
   templateUrl: './donut-card.component.html',
-  styleUrls: ['./donut-card.component.scss']
+  styleUrls: ['./donut-card.component.scss'],
 })
 export class DonutCardComponent {
-
+  @Input() donut!: Donut;
 }
