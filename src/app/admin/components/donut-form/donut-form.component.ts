a86d64c504118a564c1bf4d35aca4d06e67d5424
Jamie Thompson <jamieraethompson@gmail.com>
2023-02-15 16:26:44 -0600

    set up donut form and data binding

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { Donut } from '../../models/donut.model';
 
@@ -21,4 +22,12 @@ export class DonutFormComponent {
     'vanilla-sundae',
     'zesty-lemon',
   ];
+
+  handleSubmit(form: NgForm) {
+    if (form.valid) {
+      this.create.emit(form.value);
+    } else {
+      form.form.markAllAsTouched();
+    }
+  }
 }
--- src/app/admin/containers/donut-single/donut-single.component.ts
+++ src/app/admin/containers/donut-single/donut-single.component.ts
@@ -1,10 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Donut } from '../../models/donut.model';
+
 @Component({
   selector: 'app-donut-single',
   templateUrl: './donut-single.component.html',
   styleUrls: ['./donut-single.component.scss'],
 })
 export class DonutSingleComponent implements OnInit {
-  ngOnInit(): void {}
+  donut!: Donut;
+
+  ngOnInit(): void {
+    this.donut = {
+      id: 'y8z0As',
+      name: 'Just Chocolate',
+      icon: 'just-chocolate',
+      price: 119,
+      description: 'For the pure chocoholic.',
+    };
+  }
+
+  onCreate(donut: Donut) {
+    console.log('onCreate', donut);
+  }
 }
